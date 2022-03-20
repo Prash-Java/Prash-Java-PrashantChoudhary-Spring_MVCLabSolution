b9a4ea72ea782c2565e765f97510aae57f113531
Prash-Java <93462306+Prash-Java@users.noreply.github.com>
2022-03-20 12:14:22 +0530

    Update StudentController.java

--- main/java/com/greatlearning/smsapp_demo/controller/StudentController.java
+++ main/java/com/greatlearning/smsapp_demo/controller/StudentController.java
@@ -22,56 +22,41 @@ public class StudentController {
 	@Autowired
 	private StudentService studentService;
 
-
-
 	// add mapping for "/list"
 
 	@RequestMapping("/list")
-	public String listBooks(Model theModel) {
-
-		System.out.println("request recieved");
-		// get Books from db
+	public String listStudents(Model theModel) {
+		// get Students from db
 		List<Student> theStudents = studentService.findAll();
-		
-
 		// add to the spring model
 		theModel.addAttribute("Students", theStudents);
-
 		return "list-Students";
 	}
 
 	@RequestMapping("/showFormForAdd")
 	public String showFormForAdd(Model theModel) {
-
 		// create model attribute to bind form data
 		Student theStudent = new Student();
-
 		theModel.addAttribute("Student", theStudent);
-
 		return "Student-form";
 	}
 
 	@RequestMapping("/showFormForUpdate")
 	public String showFormForUpdate(@RequestParam("studentId") int theId,
 			Model theModel) {
-
-		// get the Book from the service
+		// get the Student from the service
 		Student theStudent = studentService.findById(theId);
-
-
-		// set Book as a model attribute to pre-populate the form
+		// set Student as a model attribute to pre-populate the form
 		theModel.addAttribute("Student", theStudent);
-
 		// send over to our form
 		return "Student-form";			
 	}
 
 
 	@PostMapping("/save")
-	public String saveBook(@RequestParam("id") int id,
+	public String save(@RequestParam("id") int id,
 			@RequestParam("firstName") String firstName,@RequestParam("lastName") String lastName,@RequestParam("course") String course,@RequestParam("country") String country) {
 
-		System.out.println(id);
 		Student theStudent;
 		if(id!=0)
 		{
@@ -83,23 +68,18 @@ public class StudentController {
 		}
 		else
 			theStudent=new Student(firstName, lastName, course,country);
-		// save the Book
+		// save the Student
 		studentService.save(theStudent);
-
-
 		// use a redirect to prevent duplicate submissions
 		return "redirect:/student/list";
-
 	}
 
 
 	@RequestMapping("/delete")
 	public String delete(@RequestParam("studentId") int theId) {
-
-		// delete the Book
+		// delete the Student
 		studentService.deleteById(theId);
-
-		// redirect to /Books/list
+		// redirect to /Student/list
 		return "redirect:/student/list";
 
 	}
